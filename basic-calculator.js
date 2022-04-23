ed976e40b9be555c889bb6a606fec616fd015e6d
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2022-04-23 07:37:57 -0400

    Create function to call operations

--- basic-calculator.js
+++ basic-calculator.js
@@ -16,4 +16,17 @@ function divide (a, b) {
   return a / b;
 }
 
-
+function operate (operator, a, b) {
+  switch (operator) {
+    case '+':
+      return add(a,b);
+    case '-':
+      return subtract(a,b);
+    case '*':
+      return multiply(a,b);
+    case '/':
+      return divide(a,b);
+    default:
+      console.log("The operate() function has failed");
+  }
+}
