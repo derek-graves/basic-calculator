e4eef6dfbee611a26d9015069516b74318561b82
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2022-04-29 07:53:28 -0400

    Consolidate operations into a single function

--- basic-calculator.js
+++ basic-calculator.js
@@ -5,35 +5,19 @@ let inActiveOperation = false;
 let backspaceValid = true;
 let display = document.querySelector('#display');
 
-function add (a, b) {
-  return a + b;
-}
-
-function subtract (a, b) {
-  return a - b;
-}
-
-function multiply (a, b) {
-  return a * b;
-}
-
-function divide (a, b) {
-  if (b === 0) {
-    return "ERROR";
-  }
-  return a / b;
-}
-
 function operate (operator, a, b) {
   switch (operator) {
     case '+':
-      return add(a,b);
+      return a + b;
     case '–':
-      return subtract(a,b);
+      return a - b;
     case '×':
-      return multiply(a,b);
+      return a * b
     case '/':
-      return divide(a,b);
+      if (b === 0) {
+        return "ERROR";
+      }
+      return a / b;
     default:
       console.log("The operate() function has failed");
   }
